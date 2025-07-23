7d474d252c92f5097150df2fb57f140bd2eeed87
Daasein <68468076+Daasein@users.noreply.github.com>
2025-07-22 21:06:46 -0400

    new block: doubly clamped beam

--- layout/blocks/beams.py
+++ layout/blocks/beams.py
@@ -1,6 +1,18 @@
 import gdsfactory as gf
 
 
+def _create_deep_etch_mask(c, mask_offset=1, x_off=True, y_off=True):
+    """Creates a deep etch mask by offsetting the polygons."""
+    polygon = c.get_polygons()[gf.get_layer("WG")][0].sized(
+        (mask_offset * 1e3 if x_off else 0, mask_offset * 1e3 if y_off else 0)
+    )
+    # add an offset polygon to create a deep etch mask (1um offset)
+    c.add_polygon(polygon, layer=(39, 0))
+    booled = gf.boolean(c, c, "not", "DEEP_ETCH", (39, 0), (1, 0))
+    c.remove_layers([(39, 0)])
+    c << booled
+
+
 def cantilever_beam(length, width, mask_offset=1):
     """Creates a cantilever beam with a deep etch mask."""
     c = gf.Component()
@@ -24,14 +36,8 @@ def cantilever_beam(length, width, mask_offset=1):
     )
     c.flatten()
 
-    polygon = c.get_polygons()[gf.get_layer("WG")][0].sized(mask_offset * 1e3)
-    # add a offset polygon to create a deep etch mask (1um offset)
+    _create_deep_etch_mask(c, mask_offset)
 
-    c.add_polygon(polygon, layer=(39, 0))
-
-    booled = gf.boolean(c, c, "not", "DEEP_ETCH", (39, 0), (1, 0))
-    c.remove_layers([(39, 0)])
-    c << booled
     c.flatten()
     c.show()
     c.add_port(
@@ -42,6 +48,51 @@ def cantilever_beam(length, width, mask_offset=1):
         layer="WG",
         port_type="placement",
     )
+
+    return c
+
+
+@gf.cell
+def doubly_clamped_beam(
+    length, width, taper_length=0.1, taper_width=0.3, mask_offset=1
+):
+    c = gf.Component()
+    beam = c << gf.components.rectangle(
+        size=(length, width), layer="WG", port_type="placement"
+    )
+    clamp_left = c << gf.components.taper(
+        length=taper_length,
+        width1=width,
+        width2=taper_width,
+        layer="WG",
+        port_types=["placement", "placement"],
+    )
+    clamp_left.connect("o1", beam.ports["e1"])
+
+    clamp_right = c << gf.components.taper(
+        length=taper_length,
+        width1=width,
+        width2=taper_width,
+        layer="WG",
+        port_types=["placement", "placement"],
+    )
+    clamp_right.connect("o1", beam.ports["e3"])
+    c.add_ports([clamp_left.ports["o2"], clamp_right.ports["o2"]])
+    c.ports[0].name = "p1"
+    c.ports[1].name = "p2"
+
+    c.add_port(
+        "m1",
+        center=(length / 2, 0),
+        orientation=-90,
+        width=1,
+        layer="WG",
+        port_type="placement",
+    )
+
+    c.flatten()
+
+    _create_deep_etch_mask(c, mask_offset=mask_offset, x_off=False)
 
     return c
 
