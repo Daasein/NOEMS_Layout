0640bd534ce5ffb8400dc00e43f625473cbc5872
Daasein <weixiangyu@protonmail.com>
2025-08-28 12:05:52 -0400

    Add a mask layer cross section, modified `ring resonator`

--- layout/blocks/cross_section.py
+++ layout/blocks/cross_section.py
@@ -24,3 +24,28 @@ def cross_section_with_sleeves(
     )
     xs = gf.CrossSection(sections=[sec3, sec1, sec2], radius=radius)
     return xs
+
+
+def cross_section_with_mask(
+    core_width,
+    total_width,
+    core_layer="WG",
+    mask_layer="DEEP_ETCH",
+    radius=50,
+):
+    """Initialize a cross-section with a core and one mask."""
+    sec1 = gf.Section(
+        width=total_width,
+        offset=0,
+        layer=mask_layer,
+        name="mask",
+    )
+    sec2 = gf.Section(
+        width=core_width,
+        offset=0,
+        layer=core_layer,
+        port_names=["o1", "o2"],
+        name="core",
+    )
+    xs = gf.CrossSection(sections=[sec2, sec1], radius=radius)
+    return xs
